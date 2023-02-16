1b0ddaf7e67859ddd1688a6ac6acf89bbd3ede3a
neha2kk3 <125586817+neha2kk3@users.noreply.github.com>
2023-02-16 21:53:02 +0530

    Add files via upload

---
+++ checking.java
@@ -0,0 +1,20 @@
+
+public class checking
+{
+    public static void main(String args[])
+    {
+        int num=25;
+        if(num>0)
+        { 
+            System.out.println(num+"numer is positive");
+        }
+        else if(num<0)
+        {
+            System.out.println(num+"number is negative");
+        }
+        else
+        {
+            System.out.println("the given number is zero");
+        }
+    }
+}
